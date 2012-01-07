bfca8e18d4c303e7805f3a7a173bee04e7410753
pollux31 <nicot.thierry@gmail.com>
2012-01-07 22:46:13 +0100

    4

---
+++ src/Constant.py
@@ -0,0 +1,24 @@
+'''
+Constant for the program
+'''
+
+import logging
+
+
+#--------------------------------------------------------------
+# Constants for the Debug
+#--------------------------------------------------------------
+
+LEVEL = logging.DEBUG
+#LEVEL = logging.INFO
+
+FILTER = True       # Filter the packet displayed
+BUFFER = False      # display octets received
+SEND   = True      # display sent packets
+CHUNK  = True      # display the chunk information
+
+#--------------------------------------------------------------
+# Constants for the Chat analyse
+#--------------------------------------------------------------
+
+BOT_NAME_NEEDED = False
